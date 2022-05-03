60cb1dbb7f4ca1b910c1be37ee3c2492df44828f
JunKoding <wnsrb0407@daum.net>
2022-05-03 16:25:25 +0900

    2022.05.03 - tabBarController 색상 변경 연습

--- tabBar2/tabBar2Controller.swift
+++ tabBar2/tabBar2Controller.swift
@@ -11,12 +11,10 @@ class tabBar2Controller: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBar.tintColor = .white
-        self.tabBar.isTranslucent = true
-        self.tabBar.barTintColor = .tintColor
-        self.tabBar.unselectedItemTintColor = .black
+        self.tabBar.tintColor = .white // 텝바 아이템을 선택했을 때 색상
+        self.tabBar.isTranslucent = true // 탭바 컨트롤러의 투명 여부
+        self.tabBar.barTintColor = .tintColor // 탭바 컨트롤러의 배경 색상
+        self.tabBar.unselectedItemTintColor = .black // 탭바 아이템을 선택하지 않았을 때 색상
     }
-
-
 }
 
